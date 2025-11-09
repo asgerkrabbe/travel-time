461f56ced6109f4e37b0a95b158bf278d8be912a
Laura Larsen <71498381+asgerkrabbe@users.noreply.github.com>
2025-11-09 15:36:09 +0000

    Fix corrupted blue test fixture image

--- deploy/test-fixtures.js
+++ deploy/test-fixtures.js
@@ -3,18 +3,18 @@ module.exports = [
     slug: 'fixture-red',
     filename: 'fixture-red.png',
     description: 'Solid red pixel',
-    base64: 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR4nGP4//8/AwAI/AL+9ZXMAAAAAElFTkSuQmCC'
+    base64: 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAACXBIWXMAAAPoAAAD6AG1e1JrAAAADElEQVR4nGP4z8AAAAMBAQDJ/pLvAAAAAElFTkSuQmCC'
   },
   {
     slug: 'fixture-green',
     filename: 'fixture-green.png',
     description: 'Solid green pixel',
-    base64: 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR4nGP8z/D/PwMDAwMD/w8ACpYD/nvNQswAAAAASUVORK5CYII='
+    base64: 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAACXBIWXMAAAPoAAAD6AG1e1JrAAAADElEQVR4nGNg+M8AAAICAQB7CYF4AAAAAElFTkSuQmCC'
   },
   {
     slug: 'fixture-blue',
     filename: 'fixture-blue.png',
     description: 'Solid blue pixel',
-    base64: 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR4nGP4z8DwHwAExgLAn6rhogAAAABJRU5ErkJggg=='
+    base64: 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAACXBIWXMAAAPoAAAD6AG1e1JrAAAADElEQVR4nGNgYPgPAAEDAQAIicLsAAAAAElFTkSuQmCC'
   }
 ];
